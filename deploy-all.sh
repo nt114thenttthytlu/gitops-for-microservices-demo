1e66dc7b52e1ba04f1b3f8872d8a9bf13e9a1b0d
moeruuu <uynthyee.313@gmail.com>
2026-05-31 01:46:25 -0700

    feat: add shopping assistant service

---
+++ deploy-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+helm install adservice ./adservice
+helm install cartservice ./cartservice
+helm install checkoutservice ./checkoutservice
+helm install currencyservice ./currencyservice
+helm install emailservice ./emailservice
+helm install frontend ./frontend
+helm install productcatalogservice ./productcatalogservice
+helm install recommendationservice ./recommendationservice
+helm install shippingservice ./shippingservice
+helm install shoppingassistant ./shoppingassistantservice
